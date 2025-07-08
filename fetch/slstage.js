31b8f5d203821cd9af1b54c0b79be1870f77ff72
aoisummer <aoisummer@users.noreply.github.com>
2025-07-09 01:13:31 +0800

    update data

--- fetch/slstage.js
+++ fetch/slstage.js
@@ -7,13 +7,17 @@ function parseData(str) {
     const { JSDOM } = require('jsdom');
     const dom = new JSDOM(str);
     const events = [];
-    
+    const now = new Date();
+
     dom.window.document.querySelectorAll('.box.history_ev.he_event').forEach((row) => {
         const summary = row.querySelector('.header .item strong').textContent;
         const times = row.querySelectorAll('.contents .time_localize');
         const dtstart = new Date(Number(times[0].getAttribute('data-ts')) * 1000);
         const dtend = new Date(Number(times[1].getAttribute('data-ts')) * 1000);
-        
+
+        if (dtstart > now) {
+            return;
+        }
         events.push({
             title: summary,
             start: dtstart.toISOString(),
